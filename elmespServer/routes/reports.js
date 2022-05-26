5b72ccc8e0f50755797990bb890ec84caf38f2cd
marksadecv <marksadecv@hotmail.com>
2022-05-26 15:07:18 -0500

    Added api endpoints query parameters support. Added utility endpoints to api

--- elmespServer/routes/reports.js
+++ elmespServer/routes/reports.js
@@ -1,28 +1,22 @@
 import express from 'express';
 import Report from '../models/report.js';
+import url from 'url';
 
 const router = express.Router();
 
-router.get('/', async (_, response) => {
+router.get('/', async (request, response) => {
     console.log('GET received');
 
+    const queryObject = url.parse(request.url, true).query;
+    console.log(queryObject);
+
     try {
-        //const reports = await Report.find();
-        const reports = await Report
-            .aggregate([
-                {$match: {carId: "JPV8523"}},
-                {$unwind: {path: '$events'}},
-                //{$match: {'events.eventType': {$gt: 3} }}
-                {$group: {
-                    _id: '$events.eventType',
-                    events: { $push: "$events" },
-                    count: { $count: { } }
-                    }}
-            ]);
+        // Get all reports (Filter by query parameter if any)
+        const reports = await Report.find(queryObject);
 
         response.json(reports);
     } catch(error) {
-        response.status(500).json({message: error.message})
+        response.status(500).json({message: error.message});
     }
 });
 
@@ -40,6 +34,45 @@ router.post('/', async (request, response) => {
         response.status(201).json(newReport);
     } catch(error) {
         response.status(400).json({message: error.message});
+    }
+});
+
+
+// UTILITY ENDPOINTS ------------------------------------------------------
+
+// Distinct operation by query parameter 'field'
+router.get('/distinct', async (request, response) => {
+    const queryObject = url.parse(request.url, true).query;
+    console.log(queryObject);
+
+    try{
+        const reports = await Report.distinct(queryObject.field);
+        response.json(reports);
+    } catch(error) {
+        response.status(500).json({message: error.message});
+    }
+});
+
+// Get a given report events 
+router.get('/events', async (request, response) => {
+    const queryObject = url.parse(request.url, true).query;
+    console.log(queryObject);
+
+    try{
+        const reports = await Report
+            .aggregate([
+                {$match: queryObject},
+                {$unwind: {path: '$events'}},
+                {$group: {
+                    _id: '$events.eventType',
+                    events: { $push: "$events" },
+                    count: { $count: { } }
+                }}
+            ]);
+        
+        response.json(reports);
+    } catch(error) {
+        response.status(500).json({message: error.message});
     }
 });
 
