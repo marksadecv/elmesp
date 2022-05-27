b1144abde23dd0aee59e4253b15eb3dfda015775
marksadecv <marksadecv@hotmail.com>
2022-05-26 23:23:10 -0500

    Basic react values binding

--- elmesp-client/src/App.js
+++ elmesp-client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function App() {
+  const [carIds, setCarIds] = React.useState(null);
+
+  const getCarIds = () => {
+    fetch('/api/reports/distinct?field=carId')
+      .then(result => result.json())
+      .then(body => setCarIds(body));
+  };
+
+
+  // Page render ---------------------
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getCarIds}>Car IDs</button>
+        {
+         <div>{carIds}</div>
+        }
     </div>
   );
 }
